898afce7dfe2f68e9f6f845ae693ac39edd712ed
balcevia <alfred.balcewicz@gmail.com>
2019-11-12 23:18:21 +0100

    initial commit

---
+++ src/main/java/com/balcewicz/time_counter/TimeCounterApplication.java
@@ -0,0 +1,13 @@
+package com.balcewicz.time_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balcewicz/timeCounter/TimeCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balcewicz.timeCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
